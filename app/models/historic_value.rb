dea022dfee8b945c162016d8833ee631d6cca8ab
Bruno Crivelaro <brunosantoscrivelaro@gmail.com>
2021-09-06 15:42:58 -0300

    fix HistoricValue numericality validations

--- app/models/historic_value.rb
+++ app/models/historic_value.rb
@@ -1,7 +1,9 @@
 class HistoricValue < ApplicationRecord
   belongs_to :valueable, polymorphic: true
 
-  validates :usd, presence: true, numericality: { only_integer: true }
-  validates :btc, presence: true, numericality: { only_integer: true }
+  validates :usd, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :btc, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :value_at, presence: true
+
+  default_scope -> { order(created_at: :desc) }
 end
--- spec/models/historic_value_spec.rb
+++ spec/models/historic_value_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe HistoricValue, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:usd) }
-    it { should validate_numericality_of(:usd).only_integer }
+    it { should validate_numericality_of(:usd).is_greater_than_or_equal_to(0) }
     it { should validate_presence_of(:btc) }
-    it { should validate_numericality_of(:btc).only_integer }
+    it { should validate_numericality_of(:btc).is_greater_than_or_equal_to(0) }
     it { should validate_presence_of(:value_at) }
   end
 end
